3f9883079cd5e98c6a433a6884e6f5dd7b6b270d
Guilherme0893 <guilhermeduarte008@gmail.com>
2021-11-12 00:18:03 -0300

    organizaçao do requisito 02 para a logica do botao, states e async function

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      //
+      loading: false,
+    };
+  }
+  // logica do event do botão
+
+  // logica do asyn
+
   render() {
+    // aqui vai entrar os states e a logica
+    // length de 3
     return (
       <div data-testid="page-login">
-        Login
+        <form>
+          <label>
+            <input
+              data-testid="login-name-input"
+            />
+          </label>
+          <button
+            type="submit"
+          >
+            Entrar
+          </button>
+        </form>
       </div>
     );
   }
