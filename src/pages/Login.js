2d102d3b9df5b4f76496f9f1eb1bf36e9edad413
Guilherme0893 <guilhermeduarte008@gmail.com>
2021-11-13 16:20:08 -0300

    organizaçao das logicas e validaçoes do input e botao

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,27 +4,64 @@ class Login extends Component {
   constructor() {
     super();
     this.state = {
-      //
+      // state original ao carregar a page
+      // é o nome do usuário e o que precisa ser capturado
+      userLogin: '',
       loading: false,
+      isButtonDisabled: true,
     };
   }
+
   // logica do event do botão
 
-  // logica do asyn
+  onInputUserChange = ({ event }) => {
+    console.log({ event });
+  }
+
+  onLoginButtonCLick = (event) => {
+    event.preventDefault();
+    console.log('O botão está funcionando!');
+  }
+
+  buttonValidationOnInput= () => {
+    // checa o input no campo do user e valida, liberando o botao
+    const { userLogin } = this.state;
+    const inputControl = 3;
+    if (userLogin.length > inputControl) {
+      // lembra que eventos alteram o STATE!!!
+      this.setState({ isButtonDisabled: false });
+      // Erro de unused associado a falta do state dentro do render e no filho!!
+    } else {
+      this.setState({ isButtonDisabled: true });
+    }
+  }
+
+  // logica do asyn?
 
   render() {
-    // aqui vai entrar os states e a logica
-    // length de 3
+    const {
+      isButtonDisabled,
+      buttonValidationOnInput,
+      onInputUserChange,
+      onLoginButtonCLick,
+    } = this.state;
     return (
       <div data-testid="page-login">
         <form>
-          <label>
-            <input
-              data-testid="login-name-input"
-            />
-          </label>
+          <input
+            data-testid="login-name-input"
+            type="text"
+            name="userLogin"
+            id="userLogin"
+            value=""
+            placeholder="User name"
+            onChange={ onInputUserChange }
+          />
           <button
             type="submit"
+            data-testid="login-submit-button"
+            onClick={ onLoginButtonCLick }
+            disable={ isButtonDisabled }
           >
             Entrar
           </button>
