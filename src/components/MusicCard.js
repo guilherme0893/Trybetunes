df972eeebc308fa4570953f51ada4a574a3ebf23
Guilherme0893 <guilhermeduarte008@gmail.com>
2021-11-17 17:12:54 -0300

    Correçao ---> Requisito 09 -- OK --

--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-// import { Link } from 'react-router-dom';
 import Loading from '../Loading';
 import { addSong, getFavoriteSongs } from '../services/favoriteSongsAPI';
 
