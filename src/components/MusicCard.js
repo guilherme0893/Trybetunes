0e5100f54e3b9496ac4dfab7955dcf0819316af8
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-06-16 14:00:12 -0300

    chore: Creates FavoriteCheckbox component to be rendered insideMusicCard

--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -1,45 +1,21 @@
 /* eslint-disable react/jsx-max-depth */
-import React, { useContext } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import Checkbox from '@mui/material/Checkbox';
-import InputLabel from '@mui/material/InputLabel';
-import {
-  addSong,
-  // getFavoriteSongs,
-} from '../services/favoriteSongsAPI';
-import GlobalContext from '../context/GlobalContext';
+import FavoriteCheckbox from './FavoriteCheckbox';
 
 function MusicCard(props) {
-  const { trackName, previewUrl, trackId } = props;
-  const {
-    setFavorite,
-  } = useContext(GlobalContext);
-
-  const handleOnCheckBoxClick = () => {
-    addSong(trackId);
-    setFavorite(true);
-  };
-
-  // const loadFavoritesSongs = () => {
-  //   const { trackId } = this.props;
-  //   getFavoriteSongs()
-  //     .then((favorites) => {
-  //       if (favorites.some((favorite) => favorite.trackId === trackId)) {
-  //         this.setState({ isCheckBoxChecked: true });
-  //       }
-  //     });
-  // }
+  const { trackName, previewUrl } = props;
 
   return (
     <Box>
       <Card
         sx={ {
           maxWidth: 450,
-          maxHeight: 150,
+          maxHeight: 180,
           m: 2,
           boxShadow: 2,
           display: 'column',
@@ -58,16 +34,7 @@ function MusicCard(props) {
               <code>audio</code>
               .
             </audio>
-            <InputLabel htmlFor="trackId" sx={ { ml: 2 } }>
-              Favorite
-              <Checkbox
-                id={ trackId }
-                type="checkbox"
-                data-testid={ `checkbox-music-${trackId}` }
-                // checked={ setFavorite }
-                onChange={ handleOnCheckBoxClick }
-              />
-            </InputLabel>
+            <FavoriteCheckbox />
           </Box>
         </CardContent>
       </Card>
@@ -78,7 +45,6 @@ function MusicCard(props) {
 MusicCard.propTypes = {
   trackName: PropTypes.string.isRequired,
   previewUrl: PropTypes.string.isRequired,
-  trackId: PropTypes.string.isRequired,
 };
 
 export default MusicCard;
