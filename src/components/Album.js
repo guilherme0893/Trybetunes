30f86b1b46efe1f9b133ee4e3e69cefcf20115f9
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-06-16 12:12:16 -0300

    chore: Creates AlbumResult component for rendering in Album component

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,17 +1,13 @@
 /* eslint-disable no-nested-ternary */
 import React, { useContext } from 'react';
 import { Box } from '@mui/material';
-import AlbumCard from './AlbumCard';
+import AlbumResult from './AlbumResult';
 import GlobalContext from '../context/GlobalContext';
 
 function Album() {
   const {
-    album,
     searched,
-    artistName,
   } = useContext(GlobalContext);
-
-  // console.log(album);
 
   return (
     <Box
@@ -20,48 +16,12 @@ function Album() {
       } }
     >
       {
-        !searched
-          // eslint-disable-next-line operator-linebreak
-          ?
-          (
-            <>
-            </>
-          )
-          // eslint-disable-next-line operator-linebreak
-          :
-          (
-            album.length !== 0 ? (
-              <Box>
-                <header
-                  style={ { textAlign: 'center' } }
-                >
-                  <h3 sx={ { } }>
-                    Results for
-                    {' '}
-                    {artistName}
-                  </h3>
-                </header>
-                <Box
-                  sx={ {
-                    display: 'flex',
-                    flexWrap: 'wrap',
-                    justifyContent: 'center',
-                  } }
-                >
-                  {album.map((a) => (
-                    <AlbumCard
-                      key={ a.collectionId }
-                      albumCover={ a.artworkUrl100 }
-                      albumName={ a.collectionName }
-                      albumId={ a.collectionId }
-                      artistName={ a.artistName }
-                    />
-                  ))}
-                </Box>
-              </Box>
-            ) : (
-              <h3 style={ { textAlign: 'center' } }>No album found</h3>
-            )
+        !searched ? (
+          <>
+          </>
+        )
+          : (
+            <AlbumResult />
           )
       }
     </Box>
