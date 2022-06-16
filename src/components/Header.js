677da5f6baf7c5bc790da7764e683108e4c893c3
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-06-16 15:19:20 -0300

    chore: Adds history push to profile and removes unused tags

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import { Toolbar, AppBar } from '@mui/material/';
-import MenuItem from '@mui/material/MenuItem';
-import Menu from '@mui/material/Menu';
 import AudiotrackIcon from '@mui/icons-material/Audiotrack';
 import IconButton from '@mui/material/IconButton';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import SearchComponent from './SearchComponent';
 
 function Header() {
+  const history = useHistory();
+  const onProfileButtonClick = () => {
+    history.push('/profile');
+  };
   return (
     <AppBar
       sx={ { width: '100%' } }
@@ -30,26 +32,10 @@ function Header() {
             fontSize: 60,
             backgroundColor: 'white',
           } }
+          onClick={ onProfileButtonClick }
         >
           <AccountCircle />
         </IconButton>
-        <Menu
-          open={ false }
-          style={ { backgroundColor: 'white' } }
-        >
-          <MenuItem>
-            <p>Search</p>
-            <Link to="/search" data-testid="link-to-search" />
-          </MenuItem>
-          <MenuItem>
-            <p>Favorites</p>
-            <Link to="/favorites" data-testid="link-to-favorites" />
-          </MenuItem>
-          <MenuItem>
-            <p>Profile</p>
-            <Link to="/profile" data-testid="link-to-profile" />
-          </MenuItem>
-        </Menu>
       </Toolbar>
     </AppBar>
   );
