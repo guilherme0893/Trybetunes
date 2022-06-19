ddbf4590250217833418a4fc96f728773b5215a4
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-06-19 17:48:44 -0300

    style: fix responsive styles in Album and related components

--- src/components/AlbumCard.js
+++ src/components/AlbumCard.js
@@ -5,9 +5,11 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
+// import GlobalContext from '../context/GlobalContext';
 
 function AlbumCard(props) {
   const { albumId, albumCover, albumName } = props;
+
   return (
     <Card
       sx={ {
--- src/components/AlbumResult.js
+++ src/components/AlbumResult.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-nested-ternary */
 import React, { useContext } from 'react';
 import { Box } from '@mui/material';
+import LinearProgress from '@mui/material/LinearProgress';
 import AlbumCard from './AlbumCard';
 import GlobalContext from '../context/GlobalContext';
 
@@ -8,6 +9,7 @@ function AlbumResult() {
   const {
     album,
     artistName,
+    spinner,
   } = useContext(GlobalContext);
 
   return (
@@ -17,37 +19,42 @@ function AlbumResult() {
       } }
     >
       {
-        album.length === 0 || !album ? (
-          <h3 style={ { textAlign: 'center' } }>No album found</h3>
-        ) : (
-          <Box>
-            <header
-              style={ { textAlign: 'center' } }
-            >
-              <h3 sx={ { } }>
-                Results for
-                {' '}
-                {artistName}
-              </h3>
-            </header>
-            <Box
-              sx={ {
-                display: 'flex',
-                flexWrap: 'wrap',
-                justifyContent: 'center',
-              } }
-            >
-              {album.map((a) => (
-                <AlbumCard
-                  key={ a.collectionId }
-                  albumCover={ a.artworkUrl100 }
-                  albumName={ a.collectionName }
-                  albumId={ a.collectionId }
-                  artistName={ a.artistName }
-                />
-              ))}
+        spinner === true ? (
+          <Box sx={ { display: 'flex' } }>
+            <LinearProgress />
+          </Box>) : (
+          album.length === 0 || !album ? (
+            <h3 style={ { textAlign: 'center' } }>No album found</h3>
+          ) : (
+            <Box>
+              <header
+                style={ { textAlign: 'center' } }
+              >
+                <h3 sx={ { } }>
+                  Results for
+                  {' '}
+                  {artistName}
+                </h3>
+              </header>
+              <Box
+                sx={ {
+                  display: 'flex',
+                  flexWrap: 'wrap',
+                  justifyContent: 'center',
+                } }
+              >
+                {album.map((a) => (
+                  <AlbumCard
+                    key={ a.collectionId }
+                    albumCover={ a.artworkUrl100 }
+                    albumName={ a.collectionName }
+                    albumId={ a.collectionId }
+                    artistName={ a.artistName }
+                  />
+                ))}
+              </Box>
             </Box>
-          </Box>
+          )
         )
       }
     </Box>
